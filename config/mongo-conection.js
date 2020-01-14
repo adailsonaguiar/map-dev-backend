a7f8ac1641fac37a104020908e5194d8026b710b
Adailson Aguiar <adailsonacj@live.com>
2020-01-14 17:56:59 -0300

    incluir arquivo de configuração

---
+++ config/mongo-conection.js
@@ -0,0 +1,10 @@
+const config = {
+  url:
+    'mongodb+srv://adailsonaguiar:4d41ls0n@cluster0-ftlu8.mongodb.net/map-dev?retryWrites=true&w=majority',
+  rules: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+};
+
+module.exports = config;
