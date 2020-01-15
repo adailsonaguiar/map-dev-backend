329cd726ecf5187258751324b0645262d8b86b81
Adailson Aguiar <adailsonacj@live.com>
2020-01-15 10:13:15 -0300

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const routes = require('./src/routes');
 const mongoConnection = require('./config/mongo-conection');
-
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 mongoose.connect(mongoConnection.url, mongoConnection.rules);
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
